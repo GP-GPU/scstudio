4fa6478c0ffe887de14ae7180ac28596578295a3
Boris Ranto <borix60@gmail.com>
2011-04-19 21:39:57 +0200

    Detect checkers

--- src/check/pyscuser/__init__.py
+++ src/check/pyscuser/__init__.py
@@ -1,2 +1,9 @@
-checkers = []
-#checkers = ["deadlock_checker", "livelock_checker"]
+import os
+dirlist = os.listdir(__path__[0])
+bcheckers = []
+hcheckers = []
+for i in dirlist:
+	if i[-3:] == ".py" and i[0] != '_' and 'B' in i:
+		bcheckers.append(i)
+	if i[-3:] == ".py" and i[0] != '_' and 'H' in i:
+		hcheckers.append(i)
