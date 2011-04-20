5d7d81fe37e6a29856ab23c2bbb61e1eded4d344
Boris Ranto <borix60@gmail.com>
2011-04-20 21:10:35 +0200

    Fix for python3

--- src/check/pyscuser/__init__.py
+++ src/check/pyscuser/__init__.py
@@ -1,9 +1,16 @@
 import os
+def unic(a):
+	try:
+		b = unicode(a)
+	except:
+		b = a
+	return b
+
 dirlist = os.listdir(__path__[0])
 bcheckers = []
 hcheckers = []
 for i in dirlist:
 	if i[-3:] == ".py" and i[0] != '_' and 'B' in i:
-		bcheckers.append(unicode('pyscuser.' + i[:-3]))
+		bcheckers.append(unic('pyscuser.' + i[:-3]))
 	if i[-3:] == ".py" and i[0] != '_' and 'H' in i:
-		hcheckers.append(unicode('pyscuser.' + i[:-3]))
+		hcheckers.append(unic('pyscuser.' + i[:-3]))
