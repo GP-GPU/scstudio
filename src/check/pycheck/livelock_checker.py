392b66489517c7cf04b1fc9c716d6410b7e0fe11
Boris Ranto <borix60@gmail.com>
2011-04-30 01:13:00 +0200

    Livelock should check for end node of original HMsc

--- src/check/pycheck/livelock_checker.py
+++ src/check/pycheck/livelock_checker.py
@@ -1,15 +1,15 @@
 from pysc import *
 
-def find_node_livelock(node, l = []):
+def find_node_livelock(node, original, l = []):
 	found = Set([]) # Set of HMscs that does not satisfy this condition
 	if node.ReferenceNode and node.msc and node.msc.HMsc:
 		# Found reference node that references HMsc
-		found |= find_node_livelock(node.msc.start, l)
+		found |= find_node_livelock(node.msc.start, original, l)
 	for n in node.succ:
 		if n not in l:
 			# Node that was not traversed in this path
 			l.append(n)
-			found |= find_node_livelock(n, l)
+			found |= find_node_livelock(n, original, l)
 			l.pop()
 		else:
 			# Reached same node -> cycle
@@ -19,7 +19,7 @@ def find_node_livelock(node, l = []):
 				if i.ReferenceNode:
 					ref = True
 					# Modify for end node
-					if [x for x in i.reachable if x.EndNode and x.owner == i.msc]:
+					if [x for x in i.reachable if x.EndNode and x.owner == original]:
 						# Reachable EndNode was found
 						ref = False
 			if ref:
@@ -30,9 +30,9 @@ def find_node_livelock(node, l = []):
 def find_livelock(h, l = []):
 	if h.HMsc:
 		l.append(h.start)
-		hmscs = find_node_livelock(h.start, l)
+		hmscs = find_node_livelock(h.start, original, l)
 		l.pop()
 	return hmscs
 
 def checkHMsc(hmsc, chm):
-	return list(find_node_livelock(hmsc.start, [hmsc.start]))
+	return list(find_node_livelock(hmsc.start, hmsc, [hmsc.start]))
